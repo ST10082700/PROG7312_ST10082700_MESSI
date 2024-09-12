9f5cfc9e209f8efc6074108a3ce5c06820a5c242
ST10082700 <st10082700@vcconnect.edu.za>
2024-09-12 20:42:32 +0200

    First commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PROG_ST10082700_MESSI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
